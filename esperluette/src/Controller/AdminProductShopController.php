5570ab232acef20486cda96b18cf29016deb02bb
Thibaud-Nallet <thibaud.nallet@gmail.com>
2019-10-11 23:10:25 +0200

    Page tableau de bord crée avec le design + design du tableau des produits

--- esperluette/src/Controller/AdminProductShopController.php
+++ esperluette/src/Controller/AdminProductShopController.php
@@ -17,11 +17,53 @@ class AdminProductShopController extends AbstractController
 {
     /**
      * @Route("/admin/products", name="admin_products_index")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(ProductShopRepository $repo)
     {
+       
+
+        $products = $repo->findBy([],[], 5, 0);
+        dump($products);
         return $this->render('admin/productShop/index.html.twig', [
             'products' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     * Crée un produit 
+     * @Route ("/admin/products/new", name="admin_products_new")
+     * @IsGranted("ROLE_ADMIN")
+     * @param ProductShop $product
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function create(Request $request, ObjectManager $manager)
+    {
+        $product = new ProductShop();
+
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //Prend en compte les images de l'autre entity
+            foreach ($product->getImagesShop() as $imagesShop) {
+                $imagesShop->setIdProduct($product);
+                $manager->persist($imagesShop);
+            }
+
+            $manager->persist($product);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Le produit <strong>{$product->getTitle()}</strong> à bien été crée"
+            );
+            return $this->redirectToRoute('admin_products_index');
+        }
+        return $this->render('admin/productShop/new.html.twig', [
+            'productForm' => $form->createView()
         ]);
     }
 
@@ -49,13 +91,31 @@ class AdminProductShopController extends AbstractController
             $manager->flush();
             $this->addFlash(
                 'success',
-                "Le produit <strong>{$product->getTitle()}</strong>à bien été modifié"
+                "Le produit <strong>{$product->getTitle()}</strong> à bien été modifié"
             );
         }
         return $this->render('admin/productShop/edit.html.twig', [
             'product' => $product,
             'productForm' => $form->createView(),
         ]);
+    }
+
+    /**
+     * Supprime un produit
+     * @Route("/admin/products/{id}/delete", name="admin_products_delete")
+     * @param ProductShop $product
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function delete(ProductShop $product, ObjectManager $manager) 
+    {
+        $manager->remove($product);
+        $manager->flush();
+        $this->addFlash(
+            'success',
+            "Le produit <strong>{$product->getTitle()}</strong> à bien été suprimé"
+        );
+        return $this->redirectToRoute('admin_products_index');
     }
 
 }
--- esperluette/src/Controller/ShopController.php
+++ esperluette/src/Controller/ShopController.php
@@ -30,61 +30,6 @@ class ShopController extends AbstractController
     }
 
     /**
-     * Permet de créer un produit
-     * @Route("/boutique/new", name="product_create")
-     * @IsGranted("ROLE_ADMIN")
-     * @return Response
-     */
-    public function create(Request $request, ObjectManager $manager)
-    {
-        $product = new ProductShop();
-
-        $form = $this->createForm(ProductType::class, $product);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            //Prend en compte les images de l'autre entity
-            foreach ($product->getImagesShop() as $imagesShop) {
-                $imagesShop->setIdProduct($product);
-                $manager->persist($imagesShop);
-            }
-
-            $manager->persist($product);
-            $manager->flush();
-
-            $this->addFlash(
-                'success',
-                "Le produit <strong>{$product->getTitle()}</strong>à bien été crée"
-            );
-            return $this->redirectToRoute('product_show', [
-                'slug' => $product->getSlug()
-            ]);
-        }
-        return $this->render('shop/new.html.twig', [
-            'productForm' => $form->createView()
-        ]);
-    }
-
-
-    /**
-     * Permet de supprimer un produit 
-     * @Route("/boutique/{slug}/delete", name="product_delete")
-     * @IsGranted("ROLE_ADMIN")
-     * @param ProductShop $product
-     * @param ObjectManager $manager
-     * @return void
-     */
-    public function delete(ProductShop $product, ObjectManager $manager) {
-        $manager->remove($product);
-        $manager->flush();
-        $this->addFlash(
-            'success',
-            "Le produit <strong>{$product->getTitle()}</strong> à bien été supprimé"
-        );
-        return $this->redirectToRoute("product_index");
-    }
-
-    /**
      * Permet d'afficher un produit
      * @Route("/boutique/{slug}", name="product_show") 
      * @return Response
