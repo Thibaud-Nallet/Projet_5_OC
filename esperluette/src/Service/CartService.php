f298648d8ad88d81d7e1ad864e40172883fcd292
Thibaud-Nallet <thibaud.nallet@gmail.com>
2019-10-13 14:24:57 +0200

    Mise en place du panier

---
+++ esperluette/src/Service/CartService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\ProductShopRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class CartService
+{
+
+    protected $session;
+    protected $productShopRepo;
+
+    public function __construct(SessionInterface $session, ProductShopRepository $productShopRepo)
+    {
+        $this->session = $session;
+        $this->productShopRepo = $productShopRepo;
+    }
+
+    public function add(int $id)
+    {
+        $panier = $this->session->get('panier', []);
+        if (!empty($panier[$id])) {
+            $panier[$id]++;
+        } else {
+            $panier[$id] = 1;
+        }
+        $this->session->set('panier', $panier);
+    }
+
+    public function remove(int $id)
+    {
+        $panier = $this->session->get('panier', []);
+        if (!empty($panier[$id])) {
+            unset($panier[$id]);
+        }
+        $this->session->set('panier', $panier);
+    }
+
+    public function getFullCart(): array
+    {
+        $panier = $this->session->get('panier', []);
+        $panierWithData = [];
+        foreach ($panier as $id => $quantity) {
+            $panierWithData[] = [
+                'product' => $this->productShopRepo->find($id),
+                'quantity' => $quantity,
+            ];
+        }
+        return $panierWithData;
+    }
+
+    public function getTotal()
+    {
+        $total = 0;
+        foreach ($this->getFullCart() as $item) {
+            $total += $item['product']->getPrice() * $item['quantity'];
+        }
+        return $total;
+    }
+}
