efa45a8c43743e7a950451d393910f005a30cf6f
Thibaud-Nallet <thibaud.nallet@gmail.com>
2019-10-15 17:22:06 +0200

    Ajout de la pagination dans la boutique

--- esperluette/src/Controller/ShopController.php
+++ esperluette/src/Controller/ShopController.php
@@ -7,6 +7,7 @@ use App\Entity\ProductShop;
 use App\Service\CartService;
 use App\Repository\ProductShopRepository;
 use App\Repository\CategoryShopRepository;
+use App\Service\Pagination;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,15 +15,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ShopController extends AbstractController
 {
     /**
-     * @Route("/boutique", name="product_index")
+     * @Route("/boutique/{page<\d+>?1}", name="product_index")
      */
-    public function shop(ProductShopRepository $productShopRepo, CategoryShopRepository $categoryShopRepo, CartService $cartService)
+    public function shop(ProductShopRepository $productShopRepo, CategoryShopRepository $categoryShopRepo, CartService $cartService, Pagination $pagination, $page)
     {
+        $pagination->setEntityClass(ProductShop::class)
+            ->setPage($page)
+            ->setLimit(9);
+
         return $this->render('shop/shop.html.twig', [
             'products' => $productShopRepo->findAll(),
             'categories' => $categoryShopRepo->findAll(),
             'productTotal' => $cartService->getProductTotal(),
-            'total' => $cartService->getTotal()
+            'total' => $cartService->getTotal(),
+            'pagination' => $pagination
         ]);
     }
 
